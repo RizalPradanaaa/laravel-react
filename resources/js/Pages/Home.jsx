71ad7fa7a3d2f762b891273eec9aaf12afa88fc8
RizalPradanaaa <rizalpradanaaaa@gmail.com>
2024-06-03 14:44:38 +0700

    Layouts

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <h1 className="title">Home</h1>;
+import Layout from "../Layouts/Layout";
+
+function Home({ name }) {
+  return <h1 className="title">Hello {name}</h1>;
 }
+
+// Persistent layouts
+// Home.layout = (page) => <Layout children={page} title="Welcome" />;
+
+export default Home;
